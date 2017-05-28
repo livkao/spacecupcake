80a964c3547ae558b7d25322afd4611656749bd6
Oliviamacguffin <livkao.lea@gmail.com>
2017-05-28 14:24:07 +0800

    Added Post CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def new	
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Good job, Liv! Your article was successfully saved <3"
+		else
+			render 'new', notice: "Sorry, Liv. Article wasn't saved."
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Meow! Your article was successfully saved!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+end
