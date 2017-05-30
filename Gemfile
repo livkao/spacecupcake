90de4515970723a76e6d154f5e1a96b7f9e3fe19
Oliviamacguffin <livkao.lea@gmail.com>
2017-05-30 15:07:23 +0800

    Setup contact form

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'pygments.rb', '~> 0.6.3'
 gem 'redcarpet', '~> 3.3', '>= 3.3.4'
 gem 'friendly_id', '~> 5.1.0'
 gem 'will_paginate', '~> 3.1', '>= 3.1.5'
+gem 'mail_form'
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :projects
+  resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
+
+  get '*path' => redirect('/')
 end
